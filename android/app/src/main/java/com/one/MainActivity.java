94346e4b61abf60f9c017dd571d5857b9e0ebc92
jay <jay.wang.scorpion@gmail.com>
2016-08-11 13:41:58 +0800

    :+1: upgrade RN-31 :+1:

--- android/app/src/main/java/com/one/MainActivity.java
+++ android/app/src/main/java/com/one/MainActivity.java
@@ -4,10 +4,6 @@ import com.facebook.react.ReactActivity;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends ReactActivity {
 
@@ -18,28 +14,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "ONE";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNDeviceInfo(),
-            new ImagePickerPackage()
-        );
     }
 }
